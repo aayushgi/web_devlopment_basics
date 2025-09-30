054037bbed41ddce4642d3f5f1904a3a8749a5ea
Ayush saxena <155671320+aayushgi@users.noreply.github.com>
2025-09-30 16:18:27 +0530

    Update app.js

--- javascript/app.js
+++ javascript/app.js
@@ -1,4 +1,20 @@
-console.log("hellow")
-let a= 6;
-let b= 5;
-console.log("sum is :", a+b)
+// console.log("hellow")
+// let a= 6;
+// let b= 5;
+// console.log("sum is :", a+b)
+ let pencil = 10;
+ let pen = 5;
+//  console.log("total is :", pencil+pen)
+let output = `the total prize is : ${pencil+pen} rupees`;
+console.log(output);
+//arithmatic operators
+let x = 10;
+let y = 5;
+let a = 5;
+console.log("the value of x+y is :", x+y);
+console.log("the value of x+y is :", x*y);
+console.log("the value of x+y is :", x-y);
+console.log("a++");
+console.log("++a");
+console.log("a++");
+console.log("++a");
